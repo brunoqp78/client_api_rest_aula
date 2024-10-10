1d797d494d2c6580a033340c1c2276206125c215
bruno queiroz <brunoqp78@gmail.com>
2024-10-10 19:23:17 -0300

    Criar uma entity básica

--- src/main/java/org/iftm/client_api_rest/ClientApiRestApplication.java
+++ src/main/java/org/iftm/client_api_rest/ClientApiRestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClientApiRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApiRestApplication.class, args);
+		SpringApplication.run(ClientApiRestApplication.class, args);		
 	}
 
 }
--- src/main/java/org/iftm/client_api_rest/entities/Client.java
+++ src/main/java/org/iftm/client_api_rest/entities/Client.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,6 +14,8 @@ import jakarta.persistence.Table;
 public class Client implements Serializable{
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
